bc8bb9df22ce8e0d73f3e2bffd73aa0372ed5efa
harishchilamkurthi <harishchil679@gmail.com>
2018-05-24 15:07:18 -0500

    onOpenMap() to create SetLocationPage,bt alone !work.need cnfign 4 gmap

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ModalController } from 'ionic-angular';
+import { SetLocationPage } from '../set-location/set-location';
 
 @IonicPage()
 @Component({
@@ -7,10 +9,13 @@ import { NgForm } from '@angular/forms';
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+  constructor(private modalCtrl: ModalController)}{}
+
   onSubmit(form:NgForm){
     console.log(form.value);
   }
   onOpenMap(){
-    
+    const modal = this.modalCtrl.create(SetLocationPage);
+    modal.present();
   }
 }
