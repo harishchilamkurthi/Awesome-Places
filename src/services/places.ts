a5d1c7b35ed9c974c43e95eb452bee3c66789994
harishchilamkurthi <harishchil679@gmail.com>
2018-05-30 19:19:10 -0500

    service to permanently store the data

---
+++ src/services/places.ts
@@ -0,0 +1,19 @@
+import { Place } from "../models/place";
+
+export class PlacesServices{
+  // we do have the same places in models, so we need to import them here.
+  private places: Place[] = [];
+
+  addPlace(title: string, 
+           description: string, 
+           location: Location, 
+           imageUrl: string)
+           {
+            const place = new Place(title, description, location, imageUrl);
+            this.places.push(place);
+           }
+
+  loadPlaces(){
+    return this.places.slice();
+  }         
+}
